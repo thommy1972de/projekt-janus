288afe2597506a1f2b11dfbc87407feee03ccd17
thommy1972de <42720290+thommy1972de@users.noreply.github.com>
2021-06-20 10:08:06 +0200

    Daten entfernt

--- db/conn.php
+++ db/conn.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(E_ALL);
- $db_server = 'rdbs5.xenonserver.de';
- $db_name = 'k103238web_paradise';
- $db_username = 'k103238web_parad';
- $db_passwort = 'n2Faf#qOxm#LMgEO4Pz@J1';
- $secret = 'GpNP1vCDdfgTzhMmt4euGzf3B0sqclyeeTT2cocKT9hEMXFB';
+ $db_server = '';
+ $db_name = '';
+ $db_username = '';
+ $db_passwort = '';
+ $secret = '';
 
 try {
 $pdo = new PDO('mysql:host='.$db_server.';dbname='.$db_name, $db_username, $db_passwort);
