4ba908326b7a338005263b30cf48460de28b1bc4
thommy1972de <42720290+thommy1972de@users.noreply.github.com>
2021-06-20 10:08:37 +0200

    Daten entfernt

--- db/server.php
+++ db/server.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(E_ALL);
- $db_server2 = 'rdbs5.xenonserver.de';
- $db_name2 = 'k103238web_test3';
- $db_username2 = 'k103238web_test3';
- $db_passwort2 = 'NojFirvyencut7';
- $secret2 = 'GpNP1vCDdfgTzhMmt4euGzf3B0sqclyeeTT2cocKT9hEMXFB';
+ $db_server2 = '';
+ $db_name2 = '';
+ $db_username2 = '';
+ $db_passwort2 = '';
+ $secret2 = '';
 
 try {
 $pdo_server = new PDO('mysql:host='.$db_server2.';dbname='.$db_name2, $db_username2, $db_passwort2);
