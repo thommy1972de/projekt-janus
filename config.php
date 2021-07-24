40c6f5bb7013d8947b37691d221bbf64a12d172d
thommy1972de <42720290+thommy1972de@users.noreply.github.com>
2021-07-24 20:29:14 +0200

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define ('DB_SERVER' = 'rdbs5.xenonserver.de');
-define ('DB_NAME' = 'k103238web_paradise');
-define ('DB_USERNAME' = 'k103238web_parad');
-define ('DB_PASSWORT' = 'n2Faf#qOxm#LMgEO4Pz@J1');
-
-define ("SITENAME", "The Dragons");
-?>
